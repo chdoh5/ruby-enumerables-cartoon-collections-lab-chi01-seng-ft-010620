d5d4f6298f84d37502541c53ce60131c6a7ffd7d
Christine Doherty <christineadoherty@gmail.com>
2019-12-20 21:14:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,5 +13,6 @@ end
 def find_the_cheese(array)
   if array.include?("cheddar" || "gouda" || "camembert")
     return array.find(array) {|food| food == "cheddar" || food == "gouda" || food == "camembert" }
+  end
 else nil 
 end
